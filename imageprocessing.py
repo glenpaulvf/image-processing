608384ee59a960cf7554c2acba5f43ed58bff9c7
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-11-14 01:33:16 -0800

    Generate i.jpg

--- imageprocessing.py
+++ imageprocessing.py
@@ -1,5 +1,3 @@
-#%matplotlib inline
-
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
@@ -9,10 +7,6 @@ import numpy as np
 imga = mpimg.imread('a.jpg')
 imgb = mpimg.imread('b.jpg')
 
-print imga.shape
-print [imga.min(), imga.max()]
-print imga.dtype
-
 imgc_1 = imga[0:250, 0:600]
 imgc_2 = imga[250:650, 0:100]
 imgc_3 = imgb
@@ -21,4 +15,22 @@ imgc_5 = imga[650:900, 0:600]
 imgc_234 = np.hstack((imgc_2, imgc_3, imgc_4))
 imgc = np.vstack((imgc_1, imgc_234, imgc_5))
 
+#plt.imshow(imgc)
 mpimg.imsave('c.jpg', imgc)
+
+
+# Generate i.jpg
+def convert_to_float32(img):
+    return img.astype(np.float32) / 255.0
+
+imgg = mpimg.imread('g.jpg')
+imgh = mpimg.imread('h.jpg')
+
+imgg = convert_to_float32(imgg)
+imgh = convert_to_float32(imgh)
+
+imgi = np.absolute(np.subtract(imgg, imgh))
+
+#plt.imshow(imgi)
+mpimg.imsave('i.jpg', imgi)
+
